784d3750341d2d7618ad0a9029d5c87f4206f4aa
Narin <narin1101@gmail.com>
2025-01-21 00:44:44 +0900

    fix: modify Response type (to void)

--- src/main/java/com/na/mysns/controller/response/Response.java
+++ src/main/java/com/na/mysns/controller/response/Response.java
@@ -1,15 +1,17 @@
 package com.na.mysns.controller.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 // 결과 코드값 획일화
 // generic 타입
+@Getter
 @AllArgsConstructor
 public class Response<T> {
     private String resultCode;
     private T result;
 
-    public static<T> Response<T> error(String errorCode){
+    public static Response<Void> error(String errorCode){
         return new Response<>(errorCode, null);
     }
 
